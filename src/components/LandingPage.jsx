2a3886cb91beb2e7c5921f16689ef6c8d5f6f056
Kham En <khamen28@gmail.com>
2023-02-19 07:36:22 -0500

    Change the color design of the profile picture

--- src/components/LandingPage.jsx
+++ src/components/LandingPage.jsx
@@ -2,20 +2,24 @@ import profileImageURL from "../assets/profile-picture.png";
 
 export const LandingPage = () => {
   return (
-    <div className="mt-8 md:flex">
-      <div className="border rounded-sm  border-charcoal-700 max-w-fit p-8 mx-auto md:mx-0 md:shadow-[-8px_8px_0px] md:shadow-charcoal-700">
-        <img src={profileImageURL} alt="" className="rounded-full bg-orange" />
+    <section className=" mt-8 md:mt-16 xl:mt-32 md:flex">
+      <div className="border rounded-sm  border-light-orange max-w-fit p-8 mx-auto md:mx-0 md:shadow-[-8px_8px_0px] md:shadow-light-orange">
+        <img
+          src={profileImageURL}
+          alt=""
+          className="rounded-full bg-extra-light-orange"
+        />
       </div>
       <div className="md:ml-4 md:flex md:flex-col md:justify-center">
-        <div className="mt-8 text-5xl font-bold text-center text-orange sm:text-6xl lg:text-7xl drop-shadow-md">
-          Hello! Welcome.
-        </div>
-        <p className="mt-4">
-          My name is Kham, and I'm glad to have you here. I'm a software
-          engineer who design and build things for the web, and this is where I
-          showcase my works. Please look around!
+        <h2 className="mt-8">Hello! Welcome.</h2>
+        <p className="mt-4 lg:text-lg">
+          Glad to have you here. My name is{" "}
+          <em className="text-orange">Kham</em>, and I'm a{" "}
+          <em className="text-orange">software engineer</em>. I design and build
+          things for the web, and this is where I showcase my works. Please look
+          around!
         </p>
       </div>
-    </div>
+    </section>
   );
 };
